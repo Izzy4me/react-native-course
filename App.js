4b28e06103b3f788d6c4512121ef8d3c171a9050
Mateusz Smet <mateusz.smet@gmail.com>
2019-08-17 15:18:34 +0200

    Init commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Couse Goal" style={styles.input}/>
+        <Button title="Add"/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
+});
