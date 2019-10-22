58bcad22560d48b403ad913fb4ece3f98dd10389
Mateusz Smet <mateusz.smet@gmail.com>
2019-10-22 19:55:17 +0200

    Add images on Meal Categories.

--- MealsApp/screens/CategoryMealScreen.js
+++ MealsApp/screens/CategoryMealScreen.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import { 
   Button,
+  FlatList,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
+
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {}}
+      />
+    );
+  }; 
+
+  const categoryId = props.navigation.getParam('categoryId');
+  const mealsToDisplay = MEALS.filter(
+    meal => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
-      <Button
-        title="Go to details"
-        onPress={() => {
-          props.navigation.navigate('MealDetail');
-        }}
-      />
-      <Button
-        title="Go back"
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={mealsToDisplay}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   );
