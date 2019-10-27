4312c8ccba3b805a803d40d014daa2659b079c92
Mateusz Smet <mateusz.smet@gmail.com>
2019-10-27 20:39:55 +0100

    Add Favourite Button

--- MealsApp/components/CategoryGridTile.js
+++ MealsApp/components/CategoryGridTile.js
@@ -4,7 +4,8 @@ import {
     TouchableNativeFeedback,
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Platform
 } from 'react-native';
 
 const CategoryGridTile = props => {
@@ -27,7 +28,12 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: 150
+        height: 150,
+        borderRadius: 10,
+        overflow: Platform.OS === 'android' && Platform.Version >= 21
+            ? 'hidden'
+            : 'visible',
+        elevation: 5
     },
     container: {
         flex: 1,
