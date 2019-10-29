29dcbd563425a313e963c6b8b17189be97aa9f7d
Mateusz Smet <mateusz.smet@gmail.com>
2019-10-29 21:14:44 +0100

    Mock favorite meals for Favorite Screen.

--- MealsApp/screens/FavoritesScreen.js
+++ MealsApp/screens/FavoritesScreen.js
@@ -3,14 +3,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import MealList from '../components/MealList';
+
 import { MEALS } from '../data/dummy-data';
 
-const FavoritesScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Favorites Screen!</Text>
-    </View>
-  );
+const FavoritesScreen = props => {
+  // Hardcoded 'Favorite meals' just o show functionality
+  const favoriteMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+  return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 };
 
 FavoritesScreen.navigationOptions = navData => {
@@ -19,7 +19,7 @@ FavoritesScreen.navigationOptions = navData => {
     headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
-          title="Menu!"
+          title="Menu"
           iconName="ios-menu"
           onPress={() => {
             navData.navigation.toggleDrawer();
