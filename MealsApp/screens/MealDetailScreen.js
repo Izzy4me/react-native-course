7483a02f6473f3495183afffb7cf3d5605f70442
Mateusz Smet <mateusz.smet@gmail.com>
2019-10-30 20:50:09 +0100

    MeailDetails component ready.

--- MealsApp/screens/MealDetailScreen.js
+++ MealsApp/screens/MealDetailScreen.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Button,  View, Text, StyleSheet } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Image,
+  Text,
+  Button,
+  StyleSheet
+} from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 import { MEALS } from '../data/dummy-data';
+
+const ListItem = props => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
 
@@ -11,17 +27,33 @@ const MealDetailScreen = (props) => {
   const selectedMeal = MEALS.find(meal => mealId === meal.id);
 
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button
-        title="Go back to Categories"
-        onPress={() => {
-          props.navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+
+        {/* Now we can make this dynamic mapping here!
+        We know this is not scallable, but here is not a problem
+        so we don't need FlatList  */}
+        <Text style={styles.title}>Ingredients</Text>
+        {selectedMeal.ingredients.map(ingredient => (
+          <ListItem key={ingredient}>{ingredient}</ListItem>
+        ))}
+        
+        <Text style={styles.title}>Steps</Text>
+          {selectedMeal.steps.map(step => (
+          <ListItem key={step}>{step}</ListItem>
+        ))}
+    </ScrollView>
   );
 };
+
+
+
+
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
@@ -44,10 +76,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  listItem: {
+    marginVertical: 8,
+    marginHorizontal: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 5
   }
 });
 
